66e8359ce9eb54d30612ab22d2ed314e1506bc65
kunalksingh <kunalk.singh9097@gmail.com>
2019-07-10 22:53:46 +0530

    new code1

---
+++ src/org/cts/in/KunalP.java
@@ -0,0 +1,11 @@
+package org.cts.in;
+
+public class KunalP {
+
+	public static void main(String[] args) {
+		System.out.println("smoke testing");
+		System.out.println("regression testing");
+
+	}
+
+}
